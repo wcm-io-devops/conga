76406b98cd08ad72521d6666ccd0d3c63360cdcb
sseifert <sseifert@pro-vision.de>
2015-06-09 18:57:01 +0200

    add unit tests

--- generator/src/test/java/io/wcm/devops/conga/generator/GeneratorTest.java
+++ generator/src/test/java/io/wcm/devops/conga/generator/GeneratorTest.java
@@ -19,19 +19,27 @@
  */
 package io.wcm.devops.conga.generator;
 
-import java.io.File;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.CharEncoding;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 public class GeneratorTest {
 
   private Generator underTest;
+  private File baseDir;
+  private File destDir;
 
   @Before
   public void setUp() {
-    File baseDir = new File("src/test/definitions");
-    File destDir = new File("target/generation-test");
+    baseDir = new File("src/test/definitions");
+    destDir = new File("target/generation-test");
     underTest = new Generator(
         new File(baseDir, "roles"),
         new File(baseDir, "environments"),
@@ -42,6 +50,47 @@ public class GeneratorTest {
   @Test
   public void testAllEnvironments() {
     underTest.generate();
+
+    File node1Dir = assertDirectory(destDir, "env1/node1");
+
+    File text1 = assertFile(node1Dir, "text/test.txt");
+    assertContains(text1, "defaultString: value1 äöüß", CharEncoding.ISO_8859_1);
+
+    File json1 = assertFile(node1Dir, "json/test.json");
+
+    File xml1tenant1 = assertFile(node1Dir, "xml/test.tenant1.xml");
+    File xml1tenant2 = assertFile(node1Dir, "xml/test.tenant2.xml");
+
+    File node2Dir = assertDirectory(destDir, "env1/node2");
+
+    File xml2tenant1 = assertFile(node2Dir, "xml/test.tenant1.xml");
+    File xml2tenant2 = assertFile(node2Dir, "xml/test.tenant2.xml");
+  }
+
+  private File assertDirectory(File assertBaseDir, String path) {
+    File dir = new File(assertBaseDir, path);
+    assertTrue("Directory exists: " + FileUtil.getCanonicalPath(dir), dir.exists() && dir.isDirectory());
+    return dir;
+  }
+
+  private File assertFile(File assertBaseDir, String path) {
+    File file = new File(assertBaseDir, path);
+    assertTrue("File exists: " + FileUtil.getCanonicalPath(file), file.exists() && file.isFile());
+    return file;
+  }
+
+  private void assertContains(File file, String contains) {
+    assertContains(file, contains, CharEncoding.UTF_8);
+  }
+
+  private void assertContains(File file, String contains, String charset) {
+    try {
+      String fileContent = FileUtils.readFileToString(file, charset);
+      assertTrue("File " + FileUtil.getCanonicalPath(file) + " contains: " + contains, StringUtils.contains(fileContent, contains));
+    }
+    catch (IOException ex) {
+      throw new RuntimeException("Unable to read contents from: " + FileUtil.getCanonicalPath(file), ex);
+    }
   }
 
   @Test
