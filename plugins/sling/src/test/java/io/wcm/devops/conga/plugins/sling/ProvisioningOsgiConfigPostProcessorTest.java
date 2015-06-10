6e1c97c6a65c7eeed9d8d79cb94f6ff55fae588a
sseifert <sseifert@pro-vision.de>
2015-06-10 23:24:31 +0200

    update unit test

--- plugins/sling/src/test/java/io/wcm/devops/conga/plugins/sling/ProvisioningOsgiConfigPostProcessorTest.java
+++ plugins/sling/src/test/java/io/wcm/devops/conga/plugins/sling/ProvisioningOsgiConfigPostProcessorTest.java
@@ -19,15 +19,20 @@
  */
 package io.wcm.devops.conga.plugins.sling;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import io.wcm.devops.conga.generator.spi.PostProcessorPlugin;
 import io.wcm.devops.conga.generator.util.PluginManager;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.Dictionary;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.CharEncoding;
+import org.apache.felix.cm.file.ConfigurationHandler;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
@@ -58,6 +63,28 @@ public class ProvisioningOsgiConfigPostProcessorTest {
 
     // validate
     assertFalse(provisioningFile.exists());
+
+    File mypid = new File(target, "my.pid.config");
+    assertTrue(mypid.exists());
+
+    try (FileInputStream is = new FileInputStream(mypid)) {
+      Dictionary<?, ?> config = ConfigurationHandler.read(is);
+      assertEquals("value1", config.get("stringProperty"));
+      assertArrayEquals(new String[] {
+          "v1", "v2", "v3"
+      }, (String[])config.get("stringArrayProperty"));
+      assertEquals(true, config.get("booleanProperty"));
+      assertEquals(999999999999L, config.get("longProperty"));
+    }
+
+    File myfactory_mypid = new File(target, "my.factory-my.pid.config");
+    assertTrue(myfactory_mypid.exists());
+
+    File myfactory_mypid2 = new File(target, "mode1/my.factory-my.pid2.config");
+    assertTrue(myfactory_mypid2.exists());
+
+    File mypid2 = new File(target, "mode2/my.pid2.config");
+    assertTrue(mypid2.exists());
 
   }
 
