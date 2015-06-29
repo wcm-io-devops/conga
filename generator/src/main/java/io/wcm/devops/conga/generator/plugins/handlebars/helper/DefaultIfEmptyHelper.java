37ebe99282364b97548d147dfc8fc167938c3fde
sseifert <sseifert@pro-vision.de>
2015-06-30 01:02:10 +0200

    add "defaultIfEmpty" helper

---
+++ generator/src/main/java/io/wcm/devops/conga/generator/plugins/handlebars/helper/DefaultIfEmptyHelper.java
@@ -0,0 +1,50 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2015 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.devops.conga.generator.plugins.handlebars.helper;
+
+import io.wcm.devops.conga.generator.spi.handlebars.HelperPlugin;
+
+import java.io.IOException;
+
+import com.github.jknack.handlebars.Options;
+import com.github.jknack.handlebars.helper.StringHelpers;
+
+/**
+ * Handlebars helper thats inserts a value with fallback to another if it's not set.
+ * Uses {@link StringHelpers#defaultIfEmpty}.
+ */
+public class DefaultIfEmptyHelper implements HelperPlugin<Object> {
+
+  /**
+   * Plugin/Helper name
+   */
+  public static final String NAME = "defaultIfEmpty";
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @Override
+  public CharSequence apply(Object context, Options options) throws IOException {
+    return StringHelpers.defaultIfEmpty.apply(context, options);
+  }
+
+}
---
+++ generator/src/test/java/io/wcm/devops/conga/generator/plugins/handlebars/helper/DefaultIfEmptyHelperTest.java
@@ -0,0 +1,48 @@
+/*
+ * #%L
+ * wcm.io
+ * %%
+ * Copyright (C) 2015 wcm.io
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.wcm.devops.conga.generator.plugins.handlebars.helper;
+
+import static org.junit.Assert.assertEquals;
+import io.wcm.devops.conga.generator.spi.handlebars.HelperPlugin;
+import io.wcm.devops.conga.generator.util.PluginManager;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.jknack.handlebars.Helper;
+
+public class DefaultIfEmptyHelperTest {
+
+  private Helper<Object> helper;
+
+  @SuppressWarnings("unchecked")
+  @Before
+  public void setUp() {
+    helper = new PluginManager().get(DefaultIfEmptyHelper.NAME, HelperPlugin.class);
+  }
+
+  @Test
+  public void testApply() throws Exception {
+    assertEquals("a", helper.apply("a", new MockOptions()));
+    assertEquals("a", helper.apply("a", new MockOptions("b")));
+    assertEquals("b", helper.apply(null, new MockOptions("b")));
+  }
+
+}
