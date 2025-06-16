8c3e9f6d84de42849c49c3a0693b2bfb358fce62
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-06-16 14:51:14 +0200

    CONGA Maven Plugin: Switch to JSR-330 annotations.

--- tooling/conga-maven-plugin/src/main/java/io/wcm/devops/conga/tooling/maven/plugin/GenerateMojo.java
+++ tooling/conga-maven-plugin/src/main/java/io/wcm/devops/conga/tooling/maven/plugin/GenerateMojo.java
@@ -28,7 +28,8 @@ import org.apache.maven.artifact.resolver.ResolutionErrorHandler;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
+import javax.inject.Inject;
+import javax.inject.Named;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -89,13 +90,13 @@ public class GenerateMojo extends AbstractCongaMojo {
   @Parameter(property = "project", required = true, readonly = true)
   private MavenProject project;
 
-  @Component
+  @Inject
   private org.apache.maven.repository.RepositorySystem repositorySystem;
-  @Component
+  @Inject
   private ResolutionErrorHandler resolutionErrorHandler;
-  @Component
+  @Inject
   private RepositorySystem repoSystem;
-  @Component
+  @Inject
   private BuildContext buildContext;
   @Parameter(defaultValue = "${repositorySystemSession}", readonly = true)
   private RepositorySystemSession repoSession;
